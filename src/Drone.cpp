b2fdbb6ae1f8f51a74d303ddaac92219b0145a02
Matthew Hughes <matt.hughes@live.com>
2020-02-24 13:13:18 +1100

    Added back target marker param

--- src/Drone.cpp
+++ src/Drone.cpp
@@ -38,19 +38,52 @@ struct Drone : URack::UModule {
 		NUM_LIGHTS
 	};
 
+	bool target;
+	rack::dsp::BooleanTrigger targetTrigger;
+
 	Drone() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configBiUpdate("X", X_PARAM, X_INPUT, X_ATTEN_PARAM, 0.f);
 		configBiUpdate("Y", Y_PARAM, Y_INPUT, Y_ATTEN_PARAM, 0.f);
-		configUpdate("Z", Z_PARAM, Z_INPUT, Z_ATTEN_PARAM, 0.f);
+		configBiUpdate("Z", Z_PARAM, Z_INPUT, Z_ATTEN_PARAM, 0.f);
 		configBiUpdate("Orbit", ORBIT_PARAM, ORBIT_INPUT, ORBIT_ATTEN_PARAM, 0.f);
 		configBiUpdate("Elevation", ELEVATION_PARAM, ELEVATION_INPUT, ELEVATION_ATTEN_PARAM, 0.f);
 		configUpdate("Distance", DISTANCE_PARAM, DISTANCE_INPUT, DISTANCE_ATTEN_PARAM, 0.f);
 		configActivate(ACTIVE_PARAM, ACTIVE_LIGHT, ACTIVE_INPUT);
-		configUpdate("Target", TARGET_PARAM, TARGET_INPUT);
+	}
+
+	void start() override {
+		URack::Dispatcher::send(hostNum, instanceAddress + "/Target", target);
+		lights[TARGET_LIGHT].setBrightness(target * 10.f);
 	}
 
 	void update(const ProcessArgs& args) override {
+		bool oldTarget = target;
+		// use the button to determine target status
+		if (!inputs[TARGET_INPUT].isConnected()) {
+			bool targetButton = params[TARGET_PARAM].getValue() > 0;
+			if (targetTrigger.process(targetButton))
+				target = target > 0 ? 0 : 1;
+		} else {
+			// if we have a connected target input, ignore the button
+			float targetVoltage = inputs[TARGET_INPUT].getVoltage();
+			target = targetVoltage > 0 ? 1 : 0;
+		}
+		if (oldTarget != target) {
+			lights[TARGET_LIGHT].setBrightness(target * 10.f);
+			URack::Dispatcher::send(hostNum, instanceAddress + "/Target", target);
+		}
+	}
+
+	void onLoad(json_t* rootJ) override {
+		json_t* targetJ = json_object_get(rootJ, "target");
+		if (targetJ) target = json_integer_value(targetJ);
+	}
+
+	json_t* onSave() override {
+		json_t* rootJ = json_object();
+		json_object_set_new(rootJ, "target", json_integer(target));
+		return rootJ;
 	}
 };
 
@@ -79,7 +112,7 @@ struct DroneWidget : URack::UModuleWidget {
 		addParam(createParamCentered<LEDBezel>(mm2px(Vec(16.625, 108.759)), module, Drone::ACTIVE_PARAM));
 		addChild(createLightCentered<LEDBezelLight<RedLight>>(mm2px(Vec(16.625, 108.759)), module, Drone::ACTIVE_LIGHT));
 		addParam(createParamCentered<LEDBezel>(mm2px(Vec(42.437, 108.759)), module, Drone::TARGET_PARAM));
-		addChild(createLightCentered<LEDBezelLight<RedLight>>(mm2px(Vec(42.437, 108.759)), module, Drone::TARGET_LIGHT));
+		addChild(createLightCentered<LEDBezelLight<BlueLight>>(mm2px(Vec(42.437, 108.759)), module, Drone::TARGET_LIGHT));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(17.753, 56.251)), module, Drone::X_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(40.39, 56.251)), module, Drone::Y_INPUT));
