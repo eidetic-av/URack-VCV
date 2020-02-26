94caeebb67edd6ac165b442571fe890fcc768a3e
Matthew Hughes <matt.hughes@live.com>
2020-02-26 16:15:53 +1100

    Sensible Billboard default params

--- src/Billboard.cpp
+++ src/Billboard.cpp
@@ -49,25 +49,24 @@ struct Billboard : URack::UModule {
 
 	Billboard() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configUpdate("Life", LIFE_PARAM, LIFE_INPUT, LIFE_ATTEN_PARAM, 0.f);
-		configUpdate("SpawnRate", SPAWN_RATE_PARAM, SPAWN_RATE_INPUT, SPAWN_RATE_ATTEN_PARAM, 0.f);
-		configUpdate("Size", SIZE_PARAM, SIZE_INPUT, SIZE_ATTEN_PARAM, 0.f);
+		configUpdate("Life", LIFE_PARAM, LIFE_INPUT, LIFE_ATTEN_PARAM, 3.f);
+		configUpdate("SpawnRate", SPAWN_RATE_PARAM, SPAWN_RATE_INPUT, SPAWN_RATE_ATTEN_PARAM, 5.f);
+		configUpdate("Size", SIZE_PARAM, SIZE_INPUT, SIZE_ATTEN_PARAM, 2.f);
 		configBiUpdate("Fade", FADE_PARAM, FADE_INPUT);
 		configBiUpdate("Growth", GROWTH_PARAM, GROWTH_INPUT);
-		configBiUpdate("TurbulenceScale", TURBULENCE_SCALE_PARAM, TURBULENCE_SCALE_INPUT);
+		configUpdate("TurbulenceScale", TURBULENCE_SCALE_PARAM, TURBULENCE_SCALE_INPUT, -1, 2.f);
 		configBiUpdate("ForceY", FORCE_Y_PARAM, FORCE_Y_INPUT);
 		configBiUpdate("ForceZ", FORCE_Z_PARAM, FORCE_Z_INPUT);
 		configBiUpdate("ForceX", FORCE_X_PARAM, FORCE_X_INPUT);
 		configUpdate("Turbulence", TURBULENCE_PARAM, TURBULENCE_INPUT, TURBULENCE_ATTEN_PARAM, 0.f);
 		configBiUpdate("TurbulenceDelay", TURBULENCE_DELAY_PARAM, TURBULENCE_DELAY_INPUT);
-		configUpdate("Fuzz", FUZZ_PARAM, FUZZ_INPUT);
+		configUpdate("Fuzz", FUZZ_PARAM, FUZZ_INPUT, -1, 2.f);
 		configActivate(ACTIVE_PARAM, ACTIVE_LIGHT, ACTIVE_INPUT);
 	}
 
 	void update(const ProcessArgs& args) override {
 	}
 };
-
 
 struct BillboardWidget : URack::UModuleWidget {
 	BillboardWidget(Billboard* module) {
@@ -77,7 +76,7 @@ struct BillboardWidget : URack::UModuleWidget {
 		addChild(createWidget<ScrewBlack>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewBlack>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH,	RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(28.307, 35.291)), module, Billboard::LIFE_PARAM));
 		addParam(createParamCentered<Davies1900hWhiteKnob>(mm2px(Vec(50.893, 35.291)), module, Billboard::SPAWN_RATE_PARAM));
@@ -95,8 +94,8 @@ struct BillboardWidget : URack::UModuleWidget {
 		addParam(createParamCentered<Davies1900hSmallWhiteKnob>(mm2px(Vec(9.492, 79.622)), module, Billboard::TURBULENCE_DELAY_PARAM));
 		addParam(createParamCentered<TrimpotGray>(mm2px(Vec(35.608, 97.526)), module, Billboard::TURBULENCE_ATTEN_PARAM));
 		addParam(createParamCentered<Davies1900hSmallWhiteKnob>(mm2px(Vec(25.562, 106.164)), module, Billboard::FUZZ_PARAM));
-                addParam(createParamCentered<LEDBezel>(mm2px(Vec(44.901, 108.759)), module, Billboard::ACTIVE_PARAM));
-                addChild(createLightCentered<LEDBezelLight<RedLight>>(mm2px(Vec(44.901, 108.759)), module, Billboard::ACTIVE_LIGHT));
+		addParam(createParamCentered<LEDBezel>(mm2px(Vec(44.901, 108.759)), module, Billboard::ACTIVE_PARAM));
+		addChild(createLightCentered<LEDBezelLight<RedLight>>(mm2px(Vec(44.901, 108.759)), module, Billboard::ACTIVE_LIGHT));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.492, 56.251)), module, Billboard::FADE_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(33.138, 56.251)), module, Billboard::LIFE_INPUT));
@@ -113,9 +112,8 @@ struct BillboardWidget : URack::UModuleWidget {
 		addPointCloudInput(mm2px(Vec(74.504, 108.759)), module, Billboard::POINT_CLOUD_INPUT, "PointCloudInput");
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(12.667, 111.875)), module, Billboard::FUZZ_INPUT));
 
-		addPointCloudOutput(mm2px(Vec(88.702, 108.759)), module, Billboard::POINT_CLOUD_INPUT, "PointCloudOutput");
+		addPointCloudOutput(mm2px(Vec(88.702, 108.759)), module, Billboard::POINT_CLOUD_OUTPUT, "PointCloudOutput");
 	}
 };
-
 
 Model* modelBillboard = createModel<Billboard, BillboardWidget>("Billboard");
