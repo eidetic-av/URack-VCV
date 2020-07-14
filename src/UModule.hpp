6f7f93bdfff2b14183e470bcb1e4ed3570cd663e
Matthew Hughes <matt.hughes@live.com>
2020-07-14 22:24:16 +1000

    Added horizontal slider

--- src/UModule.hpp
+++ src/UModule.hpp
@@ -463,6 +463,17 @@ namespace URack {
                 setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/TrimpotGray.svg")));
             }
         };
+
+        struct WhiteHorizontalSlider : app::SvgSlider {
+            WhiteHorizontalSlider() {
+                horizontal = true;
+                minHandlePos = app::mm2px(math::Vec(-2.25, -3.5625));
+                maxHandlePos = app::mm2px(math::Vec(24.75, -3.5625));
+                setBackgroundSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/WhiteHorizontalSlider.svg")));
+                setHandleSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/WhiteHorizontalSliderHandle.svg")));
+                box.size = background->box.size.plus(app::mm2px(math::Vec(0, 4.75)));
+            }
+        };
     };
 
 }  // namespace URack
